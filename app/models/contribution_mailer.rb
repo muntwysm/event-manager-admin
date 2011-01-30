72c3c8b9a233df97727b7346332217e6ba85c467
smuntwyler <smuntwyler@zis.ch>
2011-01-30 19:05:40 +0000

    removed mailer resources

--- app/models/contribution_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ContributionMailer < ActionMailer::Base
-  
- def contribution_added_email(contribution)
-  recipients current_user.email
-	from "ZIS Events"
-  subject "Contribution made to"
-  sent_on Time.now
-	#body {:url => "http://cold-window-955.heroku.com"} 
-end
